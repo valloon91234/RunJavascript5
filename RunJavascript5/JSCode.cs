66ef06eb10d44d632b7cc0c9c9d15191a354c05b
Valloon427428 <61017400+valloon91234@users.noreply.github.com>
2020-02-14 22:59:54 +0900

    Initial commit

---
+++ RunJavascript5/JSCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunJavascript5
+{
+    static class JSCode
+    {
+        public static readonly String JS_CRYPTO;
+        static JSCode()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (Stream stream = assembly.GetManifestResourceStream("RunJavascript5.CryptoJS.js"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                JS_CRYPTO = reader.ReadToEnd();
+            }
+        }
+        public const String AES_CALL = @"
+var enteredText = '227500';
+var _0x6722=['\x75\x37\x47\x75\x35\x70\x6F\x73\x76\x77\x44\x73\x58\x55\x6E\x56\x35\x5A\x61\x71\x34\x67\x3D\x3D','\x70\x61\x72\x73\x65','\x42\x61\x73\x65\x36\x34','\x65\x6E\x63'];
+var rkEncryptionKey=CryptoJS[_0x6722[3]][_0x6722[2]][_0x6722[1]](_0x6722[0])		
+var _0x283e=['\x35\x44\x39\x72\x39\x5A\x56\x7A\x45\x59\x59\x67\x68\x61\x39\x33\x30\x61\x55\x4B\x32\x77\x3D\x3D','\x70\x61\x72\x73\x65','\x42\x61\x73\x65\x36\x34','\x65\x6E\x63'];
+var rkEncryptionIv=CryptoJS[_0x283e[3]][_0x283e[2]][_0x283e[1]](_0x283e[0])
+var utf8Stringified = CryptoJS.enc.Utf8.parse(enteredText);
+var encrypted = CryptoJS.AES.encrypt(utf8Stringified.toString(), rkEncryptionKey, 
+{
+	mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7, iv: rkEncryptionIv
+});
+var encryptedText = encrypted.ciphertext.toString(CryptoJS.enc.Base64);";
+    }
+}
---
+++ RunJavascript5/Program.cs
@@ -0,0 +1,22 @@
+﻿using Jint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunJavascript5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsEngine = new Engine();
+            jsEngine.Execute(JSCode.JS_CRYPTO);
+            jsEngine.Execute(JSCode.AES_CALL);
+            String result = jsEngine.GetValue("encryptedText").AsString();
+            Console.WriteLine(result);
+            Console.ReadKey(false);
+        }
+    }
+}
